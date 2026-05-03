fcb7336bc88cba653f6c367b30f9d43821c3b487
sernine <mohammedbenyamna39@gmail.com>
2026-05-03 12:16:46 +0100

    add main entry files deployctl/deployctl.sh inboxctl/inboxctl.sh

---
+++ deployctl/deployctl.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+#
+# ------------------------------------------------------------------------------
+# project: deployctl-inboxctl: deployctl
+# SPDX-License-Identifier: MIT (see LICENSE)
+# Maintainer: BEN YAMNA Mohammed <iamsernine@gmail.com>
+# Repository: https://github.com/iamsernine/deployctl-inboxctl
+# ------------------------------------------------------------------------------
+#
+# deployctl/deployctl.sh - server-side CLI entrypoint for docker monolith deployements.
+# sources shared contracts and modular libraries; dispatches subcommands
